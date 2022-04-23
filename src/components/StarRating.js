26a1104d9d1d27bd7d750e14373e146094fd074c
BlooAM <adam3200@vp.pl>
2022-04-23 09:39:54 +0200

    add utilities for component

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -2,10 +2,14 @@ import React, { useState } from 'react';
 import { FaStar } from 'react-icons/fa';
 
 
+const createArray = length => [...Array(length)];
 const Star = ({selected = false, onSelect = f => f}) => (
     <FaStar color={selected ? "red" : "grey"} onClick={onSelect} />
 ); 
 
 export default function StarRating({totalStars = 5}){
+    const [selectedStars, setSelectedStars] = useState(0);
+    return (
 
+    );
 }
