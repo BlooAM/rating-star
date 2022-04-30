28061208e19ab2a4e35a1e8fa394458ef3f2aa3f
BlooAM <adam3200@vp.pl>
2022-04-30 10:05:52 +0200

    rating change feature

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import Color from './Color'
 
-export default function ColorList({ colors = [], onRemoveColor = f => f }){
+export default function ColorList({ 
+    colors = [], 
+    onRemoveColor = f => f,
+    onRateColor = f => f 
+}){
     if(!colors.length) return <div>No colors. (Add color)</div>;
     return (
-        <div>
+        <div className="color-list">
             {
-                colors.map(color => <Color key={color.id} {...color} onRemove={onRemoveColor} />)
+                colors.map(color => (
+                    <Color 
+                    key={color.id} 
+                    {...color} 
+                    onRemove={onRemoveColor}
+                    onRate={onRateColor} 
+                    />
+                    ))
             }
         </div>
     );
