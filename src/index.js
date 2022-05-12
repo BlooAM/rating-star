54ac5a791cea9eb022e5285865e75a9a7e1a60e2
BlooAM <adam3200@vp.pl>
2022-05-12 08:34:19 +0200

    add context for color data

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React, { createContext } from 'react';
+import { render } from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+export const ColorContext = createContext();
+render(
+    <React.StrictMode>
+      <ColorContext.Provider value={{ colors }}>
+        <App />
+      </ColorContext.Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
